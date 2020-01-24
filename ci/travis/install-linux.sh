2603de293d2d92e6eabae2b24ce05b5863568348
Glebanister <glabmar2001@gmail.com>
2020-01-24 17:54:56 +0300

    Add script for linux installation

---
+++ ci/travis/install-linux.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install g++-7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 40
+
+g++ --version
+c++ --version
+cpp --version
+gcc --version
+cc --version
+
+sudo apt-get install build-essential checkinstall
+
+CMAKE_VERSION=3.3.2
+CMAKE_VERSION_DIR=v3.3
+
+CMAKE_OS=Linux-x86_64
+CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
+CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
+CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
+
+wget $CMAKE_URL
+mkdir -p $CMAKE_DIR
+tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
+export PATH=$CMAKE_DIR/bin:$PATH
+rm -rf cmake-$CMAKE_VERSION*
+
+cmake --version
+
+# sudo apt-get install libgl1-mesa-dev
+# sudo apt install libglm-dev
+# sudo apt install libgles2-mesa-dev
+# sudo apt install libegl1-mesa-dev
+# sudo apt install libsdl2-dev
+
