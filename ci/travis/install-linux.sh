cbadf7da0d0354b9317644c398ece01eca5ca643
Glebanister <glabmar2001@gmail.com>
2020-02-13 21:32:24 +0300

    Mute everything in install

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install g++-7
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 40
+echo toolchain
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test > output
+echo apt-get update
+sudo apt-get update > output
+echo g++
+sudo apt-get install g++-7 > output
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40 > output
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 40 > output
 
 g++ --version
-c++ --version
-cpp --version
 gcc --version
-cc --version
 
-sudo apt-get install build-essential checkinstall
+echo checkinstall
+sudo apt-get install build-essential checkinstall > output
 
+echo cmake
 CMAKE_VERSION=3.3.2
 CMAKE_VERSION_DIR=v3.3
 
@@ -24,16 +25,19 @@ CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
 
 wget --quiet $CMAKE_URL
 mkdir -p $CMAKE_DIR
-tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
+tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR > output
 export PATH=$CMAKE_DIR/bin:$PATH
 rm -rf cmake-$CMAKE_VERSION*
 
 cmake --version
 
-sudo apt-get install libgl1-mesa-dev
-sudo apt install libglm-dev
-sudo apt install libgles2-mesa-dev
-sudo apt install libegl1-mesa-dev
-sudo apt install libsdl2-dev
+echo libraries
+sudo apt-get install libgl1-mesa-dev > output
+sudo apt install libglm-dev > output
+sudo apt install libgles2-mesa-dev > output
+sudo apt install libegl1-mesa-dev > output
+sudo apt install libsdl2-dev > output
 
-sudo apt-get install python3
+echo python
+sudo apt-get install python3 > output
+rm -rf output
