f166df385924b6b2860e62038015a81fc84fbbeb
Glebanister <glabmar2001@gmail.com>
2020-02-19 20:39:51 +0300

    Add trivial constructors for Camera2d

--- Ample/include/Camera2d.h
+++ Ample/include/Camera2d.h
@@ -7,6 +7,9 @@ namespace ample::graphics
 class Camera2d : public Camera
 {
 public:
+    Camera2d(pixel_t xView, pixel_t yView, pixel_t wView, pixel_t hView);
+    Camera2d(pixel_t wView, pixel_t hView);
+
     virtual void scaleX(double) = 0;
     virtual void scaleY(double) = 0;
     virtual void translateX(pixel_t) = 0;
--- Ample/src/Graphics/Camera2d.cpp
+++ Ample/src/Graphics/Camera2d.cpp
@@ -1,1 +1,10 @@
 #include "Camera2d.h"
+
+namespace ample::graphics
+{
+Camera2d::Camera2d(pixel_t xView, pixel_t yView, pixel_t wView, pixel_t hView)
+    : Camera(xView, yView, wView, hView) {}
+
+Camera2d::Camera2d(pixel_t wView, pixel_t hView)
+    : Camera(wView, hView) {}
+} // namespace ample::graphics
