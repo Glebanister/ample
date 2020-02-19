cb18426bbf06f0285cc2ae07523d257cccf55d6e
Glebanister <glabmar2001@gmail.com>
2020-02-19 19:13:08 +0300

    Add constructors to Viewport

--- Ample/include/Camera.h
+++ Ample/include/Camera.h
@@ -11,14 +11,18 @@ protected:
     {
         pixel_t x, y, w, h;
         Viewport(pixel_t xv, pixel_t yv, pixel_t wv, pixel_t hv); // TODO: w, h constructor
-        Viewport(); // TODO: remove this
+        Viewport(pixel_t wv, pixel_t hv);
+        Viewport(const Viewport &) = default;
         void set();
+        Viewport &operator=(const Viewport &) = default;
     };
 
 public:
     virtual void look() = 0;
     virtual void unlook() = 0;
     void setViewport(const Viewport &viewport);
+    void setViewport(pixel_t x, pixel_t y, pixel_t w, pixel_t h);
+    void setViewport(pixel_t w, pixel_t h);
 
 protected:
     Viewport _viewport;
--- Ample/src/Graphics/Camera.cpp
+++ Ample/src/Graphics/Camera.cpp
@@ -8,17 +8,26 @@ namespace ample::graphics
 Camera::Viewport::Viewport(pixel_t xv, pixel_t yv, pixel_t wv, pixel_t hv)
     : x(xv), y(yv), w(wv), h(hv) {}
 
-Camera::Viewport::Viewport()
-    : Viewport(0, 0, 0, 0) {}
+Camera::Viewport::Viewport(pixel_t wv, pixel_t hv)
+    : Viewport(0, 0, wv, hv) {}
 
 void Camera::Viewport::set()
 {
     glViewport(x, y, w, h);
-    std::cout << x << ' ' << y << ' ' << w << ' ' << h << std::endl;
 }
 
 void Camera::setViewport(const Viewport &viewport)
 {
     _viewport = viewport;
+}
+
+void Camera::setViewport(pixel_t x, pixel_t y, pixel_t w, pixel_t h)
+{
+    _viewport = Viewport(x, y, w, h);
+}
+
+void Camera::setViewport(pixel_t w, pixel_t h)
+{
+    _viewport = Viewport(w, h);
 }
 } // namespace ample::graphics
