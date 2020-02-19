cae1a0f7691c5abfe059aa828994def7cecc329a
Glebanister <glabmar2001@gmail.com>
2020-02-19 18:33:32 +0300

    Move Vector2d out from GraphicalObject.h

--- Ample/include/GraphicalObject2d.h
+++ Ample/include/GraphicalObject2d.h
@@ -3,31 +3,22 @@
 #include <vector>
 
 #include "GraphicalObject.h"
+#include "Vector2d.h"
 
-namespace graphics
+namespace ample::graphics
 {
 typedef double pixel_t;
-
-struct Vector2d
-{
-    Vector2d(pixel_t xp, pixel_t yp)
-        : x(xp), y(yp) {}
-    Vector2d()
-        : Vector2d(0.0, 0.0) {}
-
-    pixel_t x, y;
-};
 
 class GraphicalObject2d : public GraphicalObject
 {
 public:
     GraphicalObject2d(const std::vector<Vector2d> &);
 
-    virtual pixel_t getX() = 0;
-    virtual pixel_t getY() = 0;
-    virtual double getAngle() = 0;
-    virtual double getScaleX() = 0;
-    virtual double getScaleY() = 0;
+    virtual pixel_t getX() const = 0;
+    virtual pixel_t getY() const = 0;
+    virtual double getAngle() const = 0;
+    virtual double getScaleX() const = 0;
+    virtual double getScaleY() const = 0;
 
     void draw() override;
 
