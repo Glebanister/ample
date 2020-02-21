23c82f7ca03dde9ac9ec3ae6a82ac9d9e52283a6
Glebanister <glabmar2001@gmail.com>
2020-02-21 22:23:13 +0300

    Add transformations for ScreenCamera

--- Ample/include/ScreenCamera.h
+++ Ample/include/ScreenCamera.h
@@ -2,39 +2,56 @@
 
 #include "Camera2d.h"
 #include "Vector2d.h"
+#include "Vector3d.h"
 
 namespace ample::graphics
 {
-typedef double pixel_t;
+using pixel_t = double;
+using radians_t = double;
 
 class ScreenCamera : public Camera2d
 {
 public:
-    ScreenCamera(Vector2d camPos, Vector2d camSizes,
-                 Vector2d viewportPos, Vector2d viewportSizes);
+    ScreenCamera(Vector3d<pixel_t> cameraPosition, Vector2d<pixel_t> cameraSize,
+                 Vector2d<pixel_t> viewportPosition, Vector2d<pixel_t> viewportSize);
 
-    ScreenCamera(Vector2d camSizes, Vector2d viewportSizes);
-    ScreenCamera(Vector2d viewportSizes);
+    ScreenCamera(Vector2d<pixel_t> cameraPosition, Vector2d<pixel_t> cameraSize,
+                 Vector2d<pixel_t> viewportPosition, Vector2d<pixel_t> viewportSize);
+
+    ScreenCamera(Vector2d<pixel_t> cameraSize, Vector2d<pixel_t> viewportSize);
+    ScreenCamera(Vector2d<pixel_t> viewportSize);
 
     void look() override;
     void unlook() override;
 
     void scaleX(double) override;
     void scaleY(double) override;
+    void scaleZ(double) override;
+
     void translateX(pixel_t) override;
     void translateY(pixel_t) override;
-    void rotateZ(double) override;
+    void translateZ(pixel_t) override;
+
+    void rotateX(radians_t) override;
+    void rotateY(radians_t) override;
+    void rotateZ(radians_t) override;
 
     double getScaleX() const override;
     double getScaleY() const override;
+    double getScaleZ() const override;
+
     pixel_t getX() const override;
     pixel_t getY() const override;
-    double getAngleZ() const override;
+    pixel_t getZ() const override;
+
+    radians_t getAngleX() const override;
+    radians_t getAngleY() const override;
+    radians_t getAngleZ() const override;
 
 private:
-    double _angle = 0.0;
-    double _scaleX = 1.0, _scaleY = 1.0;
-    Vector2d _camSizes;
-    Vector2d _camPos{0.0, 0.0};
+    Vector2d<pixel_t> _size;
+    Vector3d<pixel_t> _position{0.0, 0.0, 0.0};
+    Vector3d<double> _scale{1.0, 1.0, 1.0};
+    Vector3d<radians_t> _angle{0.0, 0.0, 0.0};
 };
 } // namespace ample::graphics
--- Ample/src/Graphics/ScreenCamera.cpp
+++ Ample/src/Graphics/ScreenCamera.cpp
@@ -4,28 +4,35 @@
 #include "ScreenCamera.h"
 #include "Camera.h"
 #include "Vector2d.h"
+#include "Vector3d.h"
 
 namespace ample::graphics
 {
 
-ScreenCamera::ScreenCamera(Vector2d camPos, Vector2d camSizes,
-                           Vector2d viewportPos, Vector2d viewportSizes)
-    : Camera2d(viewportPos.x, viewportPos.y, viewportSizes.x, viewportSizes.y),
-      _camSizes(camSizes), _camPos(camPos) {}
+ScreenCamera::ScreenCamera(Vector3d<pixel_t> cameraPosition, Vector2d<pixel_t> cameraSize,
+                           Vector2d<pixel_t> viewportPosition, Vector2d<pixel_t> viewportSize)
+    : Camera2d(viewportPosition.x, viewportPosition.y, viewportSize.x, viewportSize.y),
+      _size(cameraSize),
+      _position(cameraPosition) {}
 
-ScreenCamera::ScreenCamera(Vector2d camSizes, Vector2d viewportSizes)
+ScreenCamera::ScreenCamera(Vector2d<pixel_t> cameraPosition, Vector2d<pixel_t> cameraSize,
+                           Vector2d<pixel_t> viewportPosition, Vector2d<pixel_t> viewportSize)
+    : Camera2d(viewportPosition.x, viewportPosition.y, viewportSize.x, viewportSize.y),
+      _size(cameraSize),
+      _position(cameraPosition.x, cameraPosition.y, 0.0) {}
+
+ScreenCamera::ScreenCamera(Vector2d<pixel_t> cameraSize, Vector2d<pixel_t> viewportSizes)
     : Camera2d(viewportSizes.x, viewportSizes.y),
-      _camSizes(camSizes) {}
+      _size(cameraSize) {}
 
-ScreenCamera::ScreenCamera(Vector2d viewportSizes)
-    : ScreenCamera(viewportSizes, viewportSizes) {}
+ScreenCamera::ScreenCamera(Vector2d<pixel_t> viewportSize)
+    : ScreenCamera(viewportSize, viewportSize) {}
 
 void ScreenCamera::look()
 {
     static bool wasInit = false;
     if (!wasInit)
     {
-        // setViewport(Viewport{0, 0, _width, _height});
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glEnable(GL_BLEND);
         glEnable(GL_TEXTURE_2D);
@@ -39,11 +46,13 @@ void ScreenCamera::look()
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.f, _camSizes.x, _camSizes.y, 0.f, 0.f, 1.f);
+    glOrtho(0.f, _size.x, _size.y, 0.f, 0.f, 1.f);
 
-    glTranslated(_camPos.x, _camPos.y, 0);
-    glScaled(_scaleX, _scaleY, 1.0);
-    glRotated(_angle, 0.0, 0.0, 1.0);
+    glTranslated(_position.x, _position.y, 0);
+    glScaled(_scale.x, _scale.y, _scale.z);
+    glRotated(_angle.x, 1.0, 0.0, 0.0);
+    glRotated(_angle.y, 0.0, 1.0, 0.0);
+    glRotated(_angle.z, 0.0, 0.0, 1.0);
 }
 
 void ScreenCamera::unlook()
@@ -53,51 +62,79 @@ void ScreenCamera::unlook()
 
 void ScreenCamera::scaleX(double coef)
 {
-    _scaleX *= coef;
+    _scale.x = coef;
 }
-
 void ScreenCamera::scaleY(double coef)
 {
-    _scaleY *= coef;
+    _scale.y = coef;
+}
+void ScreenCamera::scaleZ(double coef)
+{
+    _scale.z *= coef;
 }
 
 void ScreenCamera::translateX(pixel_t dx)
 {
-    _camPos.x += dx;
+    _position.x += dx;
+}
+void ScreenCamera::translateY(pixel_t dy)
+{
+    _position.y += dy;
+}
+void ScreenCamera::translateZ(pixel_t dz)
+{
+    _position.z *= dz;
 }
 
-void ScreenCamera::translateY(double dy)
+void ScreenCamera::rotateX(radians_t angle)
 {
-    _camPos.y += dy;
+    _angle.x += angle;
 }
-
-void ScreenCamera::rotateZ(double angle)
+void ScreenCamera::rotateY(radians_t angle)
 {
-    _angle += angle;
+    _angle.y += angle;
+}
+void ScreenCamera::rotateZ(radians_t angle)
+{
+    _angle.z += angle;
 }
 
 double ScreenCamera::getScaleX() const
 {
-    return _scaleX;
+    return _scale.x;
 }
-
 double ScreenCamera::getScaleY() const
 {
-    return _scaleY;
+    return _scale.y;
+}
+double ScreenCamera::getScaleZ() const
+{
+    return _scale.z;
 }
 
 pixel_t ScreenCamera::getX() const
 {
-    return _camPos.x;
+    return _position.x;
 }
-
 pixel_t ScreenCamera::getY() const
 {
-    return _camPos.y;
+    return _position.y;
+}
+pixel_t ScreenCamera::getZ() const
+{
+    return _position.z;
 }
 
-double ScreenCamera::getAngleZ() const
+radians_t ScreenCamera::getAngleX() const
 {
-    return _angle;
+    return _angle.x;
+}
+radians_t ScreenCamera::getAngleY() const
+{
+    return _angle.y;
+}
+radians_t ScreenCamera::getAngleZ() const
+{
+    return _angle.z;
 }
 } // namespace ample::graphics
