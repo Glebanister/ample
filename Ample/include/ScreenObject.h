4ada6d4c56f6cb104434f8c3d47b07cf5cb4dae4
Glebanister <glabmar2001@gmail.com>
2020-02-21 23:11:46 +0300

    Add transformations for ScreenObject

--- Ample/include/ScreenObject.h
+++ Ample/include/ScreenObject.h
@@ -3,23 +3,43 @@
 #include <vector>
 
 #include "GraphicalObject2d.h"
-#include "Vector2d.h"
+#include "Vector3d.h"
 
 namespace ample::graphics
 {
 class ScreenObject : public GraphicalObject2d
 {
 public:
-    ScreenObject(const std::vector<Vector2d> &);
+    ScreenObject(const std::vector<Vector2d<pixel_t>> &, Vector3d<pixel_t> position);
+    ScreenObject(const std::vector<Vector2d<pixel_t>> &);
+
     pixel_t getX() const override;
     pixel_t getY() const override;
-    double getAngle() const override;
+    pixel_t getZ() const override;
+
+    radians_t getAngleX() const override;
+    radians_t getAngleY() const override;
+    radians_t getAngleZ() const override;
+
     double getScaleX() const override;
     double getScaleY() const override;
+    double getScaleZ() const override;
+
+    void rotateX(radians_t angle);
+    void rotateY(radians_t angle);
+    void rotateZ(radians_t angle);
+
+    void translateX(pixel_t dx);
+    void translateY(pixel_t dy);
+    void translateZ(pixel_t dz);
+
+    void scaleX(double coef);
+    void scaleY(double coef);
+    void scaleZ(double coef);
 
 protected:
-    Vector2d _position;
-    double _angle = 0.0;
-    double _scaleX = 1.0, _scaleY = 1.0;
+    Vector3d<pixel_t> _position{0.0, 0.0, 0.0};
+    Vector3d<pixel_t> _scale{1.0, 1.0, 1.0};
+    Vector3d<pixel_t> _angle{0.0, 0.0, 0.0};
 };
 } // namespace ample::graphics
--- Ample/src/Graphics/ScreenObject.cpp
+++ Ample/src/Graphics/ScreenObject.cpp
@@ -3,31 +3,87 @@
 namespace ample::graphics
 {
 
-ScreenObject::ScreenObject(const std::vector<Vector2d> &shape)
+ScreenObject::ScreenObject(const std::vector<Vector2d<pixel_t>> &shape, Vector3d<pixel_t> position)
+    : GraphicalObject2d(shape), _position(position) {}
+
+ScreenObject::ScreenObject(const std::vector<Vector2d<pixel_t>> &shape)
     : GraphicalObject2d(shape) {}
 
 pixel_t ScreenObject::getX() const
 {
     return _position.x;
 }
-
 pixel_t ScreenObject::getY() const
 {
     return _position.y;
 }
-
-double ScreenObject::getAngle() const
+pixel_t ScreenObject::getZ() const
 {
-    return _angle;
+    return _position.z;
+}
+
+radians_t ScreenObject::getAngleX() const
+{
+    return _angle.x;
+}
+radians_t ScreenObject::getAngleY() const
+{
+    return _angle.y;
+}
+radians_t ScreenObject::getAngleZ() const
+{
+    return _angle.z;
 }
 
 double ScreenObject::getScaleX() const
 {
-    return _scaleX;
+    return _scale.x;
 }
-
 double ScreenObject::getScaleY() const
 {
-    return _scaleY;
+    return _scale.y;
+}
+double ScreenObject::getScaleZ() const
+{
+    return _scale.z;
+}
+
+void ScreenObject::rotateX(radians_t angle)
+{
+    _angle.x += angle;
+}
+void ScreenObject::rotateY(radians_t angle)
+{
+    _angle.y += angle;
+}
+void ScreenObject::rotateZ(radians_t angle)
+{
+    _angle.z += angle;
+}
+
+void ScreenObject::translateX(pixel_t dx)
+{
+    _position.x += dx;
+}
+void ScreenObject::translateY(pixel_t dy)
+{
+    _position.y += dy;
+}
+void ScreenObject::translateZ(pixel_t dz)
+{
+    _position.z += dz;
+}
+
+void ScreenObject::scaleX(double coef)
+{
+    _scale.x *= coef;
+}
+void ScreenObject::scaleY(double coef)
+{
+    _scale.y *= coef;
+}
+void ScreenObject::scaleZ(double coef)
+{
+    _scale.z *= coef;
 }
 } // namespace ample::graphics
