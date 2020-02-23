a381d47ccb82fecea8df44be0e1d6463e98d97cb
Glebanister <glabmar2001@gmail.com>
2020-02-23 20:16:54 +0300

    Add noise and button demonstration

--- demo/include/DemoGame.h
+++ demo/include/DemoGame.h
@@ -4,6 +4,7 @@
 #include "ScreenCamera.h"
 #include "GraphicalObject2d.h"
 #include "ScreenObject.h"
+#include "PerlinNoise.h"
 
 class DemoGame : public ample::window::WindowActivity
 {
@@ -14,9 +15,11 @@ private:
     void onAwake() override;
     void onActive() override;
 
-    ample::graphics::ScreenCamera camera{{1960, 1080}};
+    ample::graphics::ScreenCamera camera{{1960, 1080}, {1960, 1080}};
     ample::graphics::ScreenObject object{{{-20, -20},
                                           {20, -20},
                                           {20, 20},
                                           {-20, 20}}};
+    ample::random::PerlinNoise xNoise{103};
+    ample::random::PerlinNoise yNoise{23};
 };
