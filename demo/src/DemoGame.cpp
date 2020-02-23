527af8ee3317b1ad17daac864254f0807f9f0889
Glebanister <glabmar2001@gmail.com>
2020-02-23 21:03:19 +0300

    Rename arrow keys

--- demo/src/DemoGame.cpp
+++ demo/src/DemoGame.cpp
@@ -21,11 +21,11 @@ void DemoGame::onActive()
     static double speed = 3000.0;
     object.setX(1000.0 * (xNoise.noise0_1(ample::time::Clock::globalTimeMs() / speed) - 0.5));
     object.setY(1000.0 * (yNoise.noise0_1(ample::time::Clock::globalTimeMs() / speed) - 0.5));
-    if (eventManager->keyIsDown(ample::control::keysym::UP))
+    if (eventManager->keyIsDown(ample::control::keysym::ARROW_UP))
     {
         speed += 100;
     }
-    if (eventManager->keyIsDown(ample::control::keysym::DOWN))
+    if (eventManager->keyIsDown(ample::control::keysym::ARROW_DOWN))
     {
         speed -= 100;
     }
