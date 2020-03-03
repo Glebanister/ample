fd01740b976428695a25788ba110add999ed58e5
Glebanister <glabmar2001@gmail.com>
2020-03-03 18:13:12 +0300

    Handle opengl and sdl errors in exceptions

--- Ample/include/Exception.h
+++ Ample/include/Exception.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <exception>
 #include <iostream>
+#include <unordered_map>
+#include <GL/gl.h>
 
 namespace ample::exception
 {
@@ -21,6 +23,8 @@ enum class exId : size_t
     SDL_NOT_READY,
     OPENGL_NOT_FIT,
     OPENGL_SHADER,
+    OPENGL,
+    SDL,
     WINDOW_NOT_READY,
 };
 
@@ -38,6 +42,8 @@ static std::string exIdInfo[] = {
     "can't use SDL2: not initializated",
     "OpenGL context can't fit to the window",
     "OpenGL can't load shader",
+    "OpenGL error occured",
+    "SDL error occured",
     "can't use window: not ready yet",
 };
 
@@ -70,5 +76,34 @@ private:
     exId _id = exId::UNSPECIFIED;
     exType _type = exType::CRITICAL;
     std::string _message = "";
+};
+
+static std::unordered_map<int, std::string> glErrorMessage{
+    {GL_NO_ERROR, "GL_NO_ERROR No error has been recorded."},
+    {GL_INVALID_ENUM, "GL_INVALID_ENUM An unacceptable value is specified for an enumerated argument."},
+    {GL_INVALID_VALUE, "GL_INVALID_VALUE A numeric argument is out of range."},
+    {GL_INVALID_OPERATION, "GL_INVALID_OPERATION The specified operation is not allowed in the current state."},
+    {GL_INVALID_FRAMEBUFFER_OPERATION, "GL_INVALID_FRAMEBUFFER_OPERATION The framebuffer object is not complete."},
+    {GL_OUT_OF_MEMORY, "GL_OUT_OF_MEMORY There is not enough memory left to execute the command."},
+    {GL_STACK_UNDERFLOW, "GL_STACK_UNDERFLOW An attempt has been made to perform an operation that would cause an internal stack to underflow."},
+    {GL_STACK_OVERFLOW, "GL_STACK_OVERFLOW An attempt has been made to perform an operation that would cause an internal stack to overflow."},
+};
+
+class OpenGLException : public Exception
+{
+public:
+    static void handle(const std::string &message = "");
+
+private:
+    explicit OpenGLException(const std::string &message = "");
+};
+
+class SDLException : public Exception
+{
+public:
+    static void handle(const std::string &message = "");
+
+private:
+    explicit SDLException(const std::string &message = "");
 };
 } // namespace ample::exception
