4532b337ee2c7ed7d57d23145d62928d30c91de2
Glebanister <glabmar2001@gmail.com>
2020-03-07 21:25:34 +0300

    Add assert to Debug

--- Ample/include/Debug.h
+++ Ample/include/Debug.h
@@ -4,3 +4,8 @@
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define DEBUG(expr) std::cerr << __FILENAME__ << ' ' << __LINE__ << ": " << (expr) << std::endl
+#ifdef NDEBUG
+#define ASSERT(EX)
+#else
+#define ASSERT(EX) (void)((EX) || (__assert(#EX, __FILE__, __LINE__), 0))
+#endif
