8bb2c0ad08b2e3034f14ac813a2ae42532dec46c
Glebanister <glabmar2001@gmail.com>
2020-03-22 12:49:18 +0300

    Merge preparation

--- demo/include/DemoGame.h
+++ demo/include/DemoGame.h
@@ -10,19 +10,11 @@
 #include "PerlinNoise.h"
 #include "WorldLayer2d.h"
 #include "Vector2d.h"
-#include "CameraBehavior.h"
 #include "LayeredWindowActivity.h"
-#include "SquareBehavior.h"
 #include "Scene2d.h"
 #include "ContactListener.h"
 #include "LightSource.h"
 #include "RegularPolygon.h"
-
-class MyContactListener : public ample::physics::ContactListener
-{
-    void startContact(ample::physics::Fixture &fixtureA, ample::physics::Fixture &fixtureB) override;
-    void endContact(ample::physics::Fixture &fixtureA, ample::physics::Fixture &fixtureB) override;
-};
 
 class DemoGame : public ample::graphics::LayeredWindowActivity
 {
@@ -30,31 +22,4 @@ public:
     DemoGame(ample::window::Window &window);
 
 private:
-    void leftArrow();
-    void rightArrow();
-    void upArrow();
-    void downArrow();
-
-    void onActive() override;
-    bool isAng = false;
-    ample::graphics::CameraPerspective camera{
-        {1920, 1080},
-        {0, 0},
-        {0.0, 0.0, 0.0},
-        {0.0, 0.0, 1.0},
-        60.0,
-        1920.0 / 1080.0,
-        0.1,
-        1000.0,
-    };
-    CameraBehavior cameraBeh{*this, camera};
-    ample::physics::WorldLayer2d worldLayer{{0.0f, -100.0f}};
-    ample::physics::WorldObject2d* ground;
-    ample::physics::WorldObject2d* brick1;
-    ample::physics::WorldObject2d* brick2;
-    ample::physics::WorldObject2d* brick3;
-    ample::physics::WorldObject2d* ground2;
-    MyContactListener listener;
-    ample::graphics::light::LightSource lamp;
-    ample::random::PerlinNoise noise{42};
 };
