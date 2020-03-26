c7e255ffbe1c45434d7f137a70447143ae6ab32c
Glebanister <glabmar2001@gmail.com>
2020-03-26 17:59:36 +0300

    Add basic concepts of Game2d

--- core/include/ample/Game2d.h
+++ core/include/ample/Game2d.h
@@ -1,14 +1,32 @@
 #pragma once
 
+#include <unordered_map>
+#include <memory>
+
 #include "LayeredWindowActivity.h"
+#include "WorldLayer2d.h"
+#include "Vector2d.h"
 
 namespace ample::game::game2d
 {
 class Game2d : public graphics::LayeredWindowActivity
 {
 public:
-    Game2d(window::Window &window);
+    Game2d(window::Window &window, float sliceThikness, float physicsLayerPosition,
+           const graphics::Vector2d<float> &frontSliceGravity = {0.f, -10.f});
+
+    void setSlice(physics::WorldLayer2d &slice, const size_t num);
+
+    physics::WorldLayer2d &frontSlice() noexcept;
+    physics::WorldLayer2d &numberedSlice(const size_t num);
 
 private:
+    const float _sliceThikness;
+    const float _physicsLayerPosition;
+    const graphics::Vector2d<float> _defaultGravity;
+
+    std::unordered_map<size_t, physics::WorldLayer2d *> _sliceByDistance;
+    physics::WorldLayer2d _frontSlice;
+    graphics::Layer _layout;
 };
 } // namespace ample::game::game2d
--- core/src/Game/Game2d/Game2d.cpp
+++ core/src/Game/Game2d/Game2d.cpp
@@ -1,7 +1,38 @@
+#include <string>
+
 #include "Game2d.h"
+#include "GameException.h"
 
 namespace ample::game::game2d
 {
-Game2d::Game2d(window::Window &window)
-    : graphics::LayeredWindowActivity(window) {}
+Game2d::Game2d(window::Window &window, float layerThikness, float physicsLayerPosition,
+               const graphics::Vector2d<float> &frontLayerGravity)
+    : graphics::LayeredWindowActivity(window),
+      _sliceThikness(layerThikness),
+      _physicsLayerPosition(physicsLayerPosition),
+      _frontSlice(frontLayerGravity),
+      _defaultGravity(frontLayerGravity)
+{
+    addLayer(_frontSlice);
+    _sliceByDistance[0] = &_frontSlice;
+}
+
+void Game2d::setSlice(physics::WorldLayer2d &slice, const size_t num)
+{
+    _sliceByDistance[num] = &slice;
+}
+
+physics::WorldLayer2d &Game2d::frontSlice() noexcept
+{
+    return _frontSlice;
+}
+
+physics::WorldLayer2d &Game2d::numberedSlice(const size_t num)
+{
+    if (!_sliceByDistance[num])
+    {
+        throw GameException{"wrong layer number: " + std::to_string(num)};
+    }
+    return *_sliceByDistance[num];
+}
 } // namespace ample::game::game2d
