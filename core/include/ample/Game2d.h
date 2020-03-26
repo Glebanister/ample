2564607772334937dbbc3bec03b2fabde1f70765
Glebanister <glabmar2001@gmail.com>
2020-03-26 14:13:06 +0300

    Init Game2d

---
+++ core/include/ample/Game2d.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "LayeredWindowActivity.h"
+
+namespace ample::game::game2d
+{
+class Game2d : public graphics::LayeredWindowActivity
+{
+public:
+    Game2d(window::Window &window);
+
+private:
+};
+} // namespace ample::game::game2d
---
+++ core/src/Game/Game2d/Game2d.cpp
@@ -0,0 +1,7 @@
+#include "Game2d.h"
+
+namespace ample::game::game2d
+{
+Game2d::Game2d(window::Window &window)
+    : graphics::LayeredWindowActivity(window) {}
+} // namespace ample::game::game2d
