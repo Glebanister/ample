815edab00fd4bea3d69ec0b9878c08d5f2070756
Glebanister <glabmar2001@gmail.com>
2020-03-30 01:28:06 +0300

    Demonstrate animations in demo

--- demo/include/DemoGame.h
+++ demo/include/DemoGame.h
@@ -3,13 +3,46 @@
 #include <memory>
 
 #include "Game2d.h"
+#include "KeyboardControlCamera.h"
+#include "Texture.h"
+
+class AnimationSwapper : public ample::activity::Behaviour
+{
+public:
+    AnimationSwapper(ample::graphics::Texture *texture, size_t delta)
+        : delta(delta), texture(texture)
+    {
+    }
+
+    void onActive() override
+    {
+        ample::activity::Behaviour::onActive();
+        auto curTime = ample::time::Clock::globalTimeMs();
+        if (curTime - lastTime > delta)
+        {
+            texture->nextFrame();
+            lastTime = curTime;
+        }
+    }
+
+private:
+    ample::graphics::Texture *texture;
+    size_t delta;
+    int lastTime = 0;
+};
 
 class DemoGame : public ample::game::game2d::Game2d
 {
 public:
     DemoGame(ample::window::Window &window);
 
+    void onActive() override;
+
 private:
-    std::unique_ptr<ample::graphics::GraphicalObject2d> object1;
-    std::unique_ptr<ample::graphics::GraphicalObject2d> object2;
+    ample::graphics::GraphicalObject2d object;
+    std::unique_ptr<KeyboardControlCamera> cameraRemote;
+    ample::graphics::Texture *texture;
+    ample::graphics::Texture *textureSide;
+    AnimationSwapper *swapper;
+    AnimationSwapper *swapperSide;
 };
--- demo/src/DemoGame.cpp
+++ demo/src/DemoGame.cpp
@@ -5,57 +5,54 @@
 
 #include "DemoGame.h"
 #include "RegularPolygon.h"
+#include "Texture.h"
+#include "Clock.h"
 
 DemoGame::DemoGame(ample::window::Window &window)
-    : ample::game::game2d::Game2d(window)
+    : ample::game::game2d::Game2d(window),
+      object(ample::geometry::RegularPolygon<float>(10.0, 40, M_PI_4),
+             10.0f,
+             50.0f,
+             {2.0f, 2.0f},
+             {1.0f, 10.0f},
+             ample::graphics::normalsMode::VERTEX,
+             {0.0f, 0.0f},
+             0.0f)
 {
-    ample::graphics::GraphicalObject2dRaw sample1{ample::geometry::RegularPolygon<float>(50, 8),
-                                                  10.0,
-                                                  20.0,
-                                                  "../../demo/textures/lena512.png",
-                                                  {512, 512},
-                                                  {0, 0},
-                                                  {
-                                                      ample::graphics::textureMode::STRETCH,
-                                                      ample::graphics::textureMode::STRETCH,
-                                                  },
-                                                  ample::graphics::channelMode::RGB,
-                                                  "../../demo/textures/lena512.png",
-                                                  {512, 512},
-                                                  {0, 0},
-                                                  {
-                                                      ample::graphics::textureMode::STRETCH,
-                                                      ample::graphics::textureMode::STRETCH,
-                                                  },
-                                                  ample::graphics::channelMode::RGB,
-                                                  ample::graphics::normalsMode::FACE};
-    ample::graphics::GraphicalObject2dRaw sample2{ample::geometry::RegularPolygon<float>(10, 8),
-                                                  10.0,
-                                                  20.0,
-                                                  "../../demo/textures/wood.jpg",
-                                                  {2000, 1333},
-                                                  {0, 0},
-                                                  {
-                                                      ample::graphics::textureMode::STRETCH,
-                                                      ample::graphics::textureMode::STRETCH,
-                                                  },
-                                                  ample::graphics::channelMode::RGBA,
-                                                  "../../demo/textures/LAND.BMP",
-                                                  {1024, 768},
-                                                  {0, 0},
-                                                  {
-                                                      ample::graphics::textureMode::STRETCH,
-                                                      ample::graphics::textureMode::STRETCH,
-                                                  },
-                                                  ample::graphics::channelMode::RGB,
-                                                  ample::graphics::normalsMode::FACE};
-    object1 = std::make_unique<ample::graphics::GraphicalObject2d>(sample1);
-    object2 = std::make_unique<ample::graphics::GraphicalObject2d>(sample2);
-    object2->translate({-30, 0, -30});
     _window.disableCursor();
     auto &level = createLevel(1, 10.0f, 0.5f);
-    level.frontSlice().addObject(*object1);
-    level.frontSlice().addObject(*object2);
+    level.frontSlice().addObject(object);
     setCurrentLevel(1);
-    level.camera().translate({0.0, 0.0, -50.0});
+    level.camera().translate({0.0, 10.0, 0.0});
+    cameraRemote = std::make_unique<KeyboardControlCamera>(*eventManager, level.camera());
+    addBehaviour(*cameraRemote);
+    level.frontSlice().addObject(cameraRemote->getLamp());
+    texture = new ample::graphics::Texture(ample::graphics::TextureRaw("../../demo/textures/braid.jpg",
+                                                                       {116UL, 135UL},
+                                                                       {0L, 0L},
+                                                                       {7, 4},
+                                                                       ample::graphics::channelMode::RGB,
+                                                                       ample::graphics::texturePlayback::NORMAL,
+                                                                       27));
+    object.face().bindTexture(std::shared_ptr<ample::graphics::Texture>(texture));
+    swapper = new AnimationSwapper{texture, 20};
+    textureSide = new ample::graphics::Texture(ample::graphics::TextureRaw("../../demo/textures/braid.jpg",
+                                                                           {116UL, 135UL},
+                                                                           {0L, 0L},
+                                                                           {7, 4},
+                                                                           ample::graphics::channelMode::RGB,
+                                                                           ample::graphics::texturePlayback::NORMAL,
+                                                                           27));
+    object.side().bindTexture(std::shared_ptr<ample::graphics::Texture>(textureSide));
+    swapperSide = new AnimationSwapper{textureSide, 20};
+    addBehaviour(*swapperSide);
+}
+
+void DemoGame::onActive()
+{
+    Game2d::onActive();
+    if (eventManager->keyboard()->isKeyDown(ample::control::keysym::SPACE))
+    {
+        texture->nextFrame();
+    }
 }
