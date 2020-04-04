c6e6c7015734400b69474f6d1cec7d39b97d59a4
Glebanister <glebmar2001@gmail.com>
2020-04-04 23:31:33 +0300

    Implement observer

---
+++ gui/include/Observer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ample/Behaviour.h"
+#include "ample/EventManager.h"
+#include "ample/LightSource.h"
+#include "ample/CameraPerspective.h"
+
+namespace ample::gui
+{
+
+class Observer : public ample::activity::Behavior
+{
+public:
+    Observer(ample::control::EventManager &manager, const graphics::Vector2d<int> &size = {1920, 1080});
+
+    void onActive() override;
+
+    ample::graphics::light::LightSource &getLamp();
+    ample::graphics::CameraPerspective &getCamera();
+    void onWindowResized(const graphics::Vector2d<int> &size);
+
+protected:
+    ample::control::EventManager &_manager;
+    ample::graphics::light::LightSource _lamp;
+    ample::graphics::CameraPerspective _camera;
+    const float _cf = 3.0f / 5.0f;
+};
+
+} // namespace ample::gui
