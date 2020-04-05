39148a13690e7bb53f1a1b4a8ec1aeebf04b24f2
Glebanister <glebmar2001@gmail.com>
2020-04-05 15:59:36 +0300

    Add limit template

--- core/include/ample/Utils.h
+++ core/include/ample/Utils.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <algorithm>
+
 namespace ample::utils
 {
 template <class T>
 void ignore(const T &) {}
+
+template <typename T>
+void limit(T &value, const T &min, const T &max)
+{
+    value = std::min(max, value);
+    value = std::max(min, value);
+}
 } // namespace ample::utils
