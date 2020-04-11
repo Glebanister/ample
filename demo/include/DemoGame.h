9d721d1306385d0be04246fc9ceacddbbea623a1
Glebanister <glebmar2001@gmail.com>
2020-04-11 12:45:12 +0300

    Clean up DemoGame

--- demo/include/DemoGame.h
+++ demo/include/DemoGame.h
@@ -10,70 +10,13 @@
 #include "Debug.h"
 #include "RegularPolygon.h"
 
-class Braid : public ample::graphics::GraphicalObject2d
-{
-public:
-    Braid()
-        : GraphicalObject2d("Braid",
-                            ample::geometry::RegularPolygon<float>(10.0f, 6),
-                            10.0f,
-                            0.0f,
-                            {1.0f, 1.0f},
-                            {1.0f, 1.0f},
-                            ample::graphics::normalsMode::FACE,
-                            {0.0f, 0.0f},
-                            0.0f)
-    {
-        auto texture = std::make_shared<ample::graphics::Texture>(ample::graphics::TextureRaw("../../demo/textures/braid.jpg",
-                                                                                              "Braid textre",
-                                                                                              {820UL / 7 - 1, 546UL / 4 - 1},
-                                                                                              {2L, 2L},
-                                                                                              {7, 4},
-                                                                                              ample::graphics::channelMode::RGBA,
-                                                                                              ample::graphics::texturePlayback::NORMAL,
-                                                                                              27,
-                                                                                              {
-                                                                                                  ample::graphics::textureOrigin::REVERSED,
-                                                                                                  ample::graphics::textureOrigin::REVERSED,
-                                                                                              }));
-        bindTexture(texture);
-        face().bindTexture(texture);
-        side().bindTexture(texture);
-    }
-};
-
-class BraidIdle : public ample::game::ControlledObject::ObjectState<Braid>
-{
-public:
-    BraidIdle(const std::string &name, std::shared_ptr<Braid> braid)
-        : ObjectState(name, braid) {}
-
-    void onActive() override
-    {
-        ObjectState::onActive();
-    }
-};
-
-class BraidRun : public ample::game::ControlledObject::ObjectState<Braid>
-{
-public:
-    BraidRun(const std::string &name, std::shared_ptr<Braid> braid)
-        : ObjectState(name, braid) {}
-
-    void onActive() override
-    {
-        ObjectState::onActive();
-        object()->texture()->nextFrame();
-    }
-};
-
 class DemoGame : public ample::game::game2d::Game2d
 {
 public:
     DemoGame(ample::window::Window &window);
 
 private:
-    std::shared_ptr<Braid> braid;
+    std::shared_ptr<ample::graphics::GraphicalObject2d> braid;
     std::shared_ptr<KeyboardControlCamera> cameraRemote;
     std::shared_ptr<ample::graphics::Texture> texture;
 };
