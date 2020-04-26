8140ab597f440103607a6affddec9da19c7924a5
Glebanister <glebmar2001@gmail.com>
2020-04-26 12:49:19 +0300

    Add name to TextureRaw

--- core/include/ample/NamedObject.h
+++ core/include/ample/NamedObject.h
@@ -7,7 +7,7 @@ namespace ample::filing
 class NamedObject
 {
 public:
-    NamedObject(const std::string &name = "", const std::string &className = "");
+    NamedObject(const std::string &name, const std::string &className);
 
     std::string name() const noexcept;
     std::string className() const noexcept;
