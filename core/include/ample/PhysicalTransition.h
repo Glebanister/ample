c385a99ec3309f7310abf6f2d54e652f540cc2d3
Glebanister <glebmar2001@gmail.com>
2020-04-30 23:15:52 +0300

    Implement physicalTrantion

--- core/include/ample/PhysicalTransition.h
+++ core/include/ample/PhysicalTransition.h
@@ -7,8 +7,16 @@ namespace ample::game::stateMachine::transitions
 class PhysicalTransition : public StateMachine::Transition
 {
 public:
+    PhysicalTransition(const std::string &name,
+                       const std::string &className,
+                       std::shared_ptr<StateMachine::State> nextState,
+                       const std::string &firstBodyName,
+                       const std::string &secondBodyName);
+    PhysicalTransition(const filing::JsonIO &input,
+                       std::shared_ptr<StateMachine::State> nextState);
+    std::string dump() override;
 
 private:
-    
+    std::string _firstBodyName, _secondBodyName;
 };
-} // namespace ample::game::state
+} // namespace ample::game::stateMachine::transitions
--- core/src/Game/StateMachine/Transitions/Physics/PhysicalTransition.cpp
+++ core/src/Game/StateMachine/Transitions/Physics/PhysicalTransition.cpp
@@ -1,1 +1,30 @@
 #include "PhysicalTransition.h"
+
+namespace ample::game::stateMachine::transitions
+{
+PhysicalTransition::PhysicalTransition(const std::string &name,
+                                       const std::string &className,
+                                       std::shared_ptr<StateMachine::State> nextState,
+                                       const std::string &firstBodyName,
+                                       const std::string &secondBodyName)
+    : Transition(name, className, nextState),
+      _firstBodyName(std::move(firstBodyName)),
+      _secondBodyName(std::move(secondBodyName))
+{
+}
+PhysicalTransition::PhysicalTransition(const filing::JsonIO &input,
+                                       std::shared_ptr<StateMachine::State> nextState)
+    : PhysicalTransition(input.read<std::string>("name"),
+                         input.read<std::string>("class_name"),
+                         nextState,
+                         input.read<std::string>("first_body_name"),
+                         input.read<std::string>("second_body_name")) {}
+
+std::string PhysicalTransition::dump()
+{
+    filing::JsonIO result = Transition::dump();
+    result.write<std::string>("first_body_name", _firstBodyName);
+    result.write<std::string>("second_body_name", _secondBodyName);
+    return result;
+}
+} // namespace ample::game::stateMachine::transitions
