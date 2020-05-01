07cab08989101c455ac9c24a4ca7e69a928135c2
Gleb <42495111+Glebanister@users.noreply.github.com>
2020-05-01 20:09:30 +0300

    Update DemoGame.cpp

--- demo/src/DemoGame.cpp
+++ demo/src/DemoGame.cpp
@@ -17,14 +17,14 @@ DemoGame::DemoGame(ample::window::Window &window)
 {
     firstLevel = Game2d::createLevel("first", 10.0f, 0.5f, {0.0f, -10.0f}, "first_level");
     setCurrentLevel(firstLevel);
-    auto obj = std::shared_ptr<ample::physics::WorldObject2d>(new ample::physics::WorldObject2d("ground",
-                                                                                                firstLevel->frontSlice(),
-                                                                                                ample::physics::BodyType::STATIC_BODY,
-                                                                                                {{30, 10}, {30, -5}, {-30, -5}, {-30, 2}},
-                                                                                                1.0,
-                                                                                                {10.0f, 5.0f},
-                                                                                                {1.0f, 10.0f},
-                                                                                                ample::graphics::normalsMode::FACE,
-                                                                                                {-50, -50}));
+    // auto obj = std::shared_ptr<ample::physics::WorldObject2d>(new ample::physics::WorldObject2d("ground",
+    //                                                                                             firstLevel->frontSlice(),
+    //                                                                                             ample::physics::BodyType::STATIC_BODY,
+    //                                                                                             {{30, 10}, {30, -5}, {-30, -5}, {-30, 2}},
+    //                                                                                             1.0,
+    //                                                                                             {10.0f, 5.0f},
+    //                                                                                             {1.0f, 10.0f},
+    //                                                                                             ample::graphics::normalsMode::FACE,
+    //                                                                                             {-50, -50}));
     firstLevel->save();
 }
