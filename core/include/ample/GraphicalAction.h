3f10db1816187671715d6b8e0c4117359ee343d1
Glebanister <glebmar2001@gmail.com>
2020-05-01 14:19:30 +0300

    Implement Graphical and Physical actions

---
+++ core/include/ample/GraphicalAction.h
@@ -0,0 +1,7 @@
+#include "ObjectGroupAction.h"
+#include "GraphicalObject.h"
+
+namespace ample::game::stateMachine::actions
+{
+using PhysicalAction = ObjectGroupAction<graphics::GraphicalObject>;
+} // namespace ample::game::stateMachine::actions
---
+++ core/include/ample/PhysicalAction.h
@@ -0,0 +1,7 @@
+#include "ObjectGroupAction.h"
+#include "WorldObject2d.h"
+
+namespace ample::game::stateMachine::actions
+{
+using PhysicalAction = ObjectGroupAction<physics::WorldObject2d>;
+} // namespace ample::game::stateMachine::actions
