79a0aee516a32f8b1cc8f6bd4c67c2d4e8ee37f4
Glebanister <glebmar2001@gmail.com>
2020-05-05 03:54:16 +0300

    Implement clearing color setter

--- core/include/ample/Color.h
+++ core/include/ample/Color.h
@@ -8,6 +8,7 @@ struct Color
 {
     float r, g, b, a;
     Color(int32_t red, int32_t green, int32_t blue, int32_t alpha = 255);
+    Color(float red, float green, float blue, float alpha = 1.0f);
     Color(int32_t hex, int32_t alpha = 255);
     Color();
 };
