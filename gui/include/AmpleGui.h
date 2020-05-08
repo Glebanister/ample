37d4f268f3d5075a413ebec280efbbca2cad6242
Glebanister <glebmar2001@gmail.com>
2020-05-08 17:23:37 +0300

    Reimplement AmpleGui

--- gui/include/AmpleGui.h
+++ gui/include/AmpleGui.h
@@ -2,7 +2,8 @@
 
 #include <optional>
 
-#include <imgui.h>
+#include <imgui.h> // sould be included before imfilebrowser.h
+
 #include <imfilebrowser.h>
 
 #include "ample/Behaviour.h"
@@ -16,13 +17,21 @@
 #include "Editor.h"
 #include "ImguiActivity.h"
 #include "ObjectConstructor.h"
-#include "Observer.h"
 #include "ObjectStorageGui.h"
+#include "Observer.h"
 
 namespace ample::gui
 {
 class AmpleGui : public utils::Singleton<AmpleGui>, public ImguiActivity
 {
+    enum status
+    {
+        NOT_STARTED = 0,
+        CREATING,
+        OPENING,
+        SAVING_AS
+    };
+
 public:
     AmpleGui();
 
@@ -33,6 +42,8 @@ public:
     void openProject(const std::filesystem::path &path);
     void createNewProject(const std::filesystem::path &);
 
+    void usePathSelector(status);
+
 private:
     std::shared_ptr<game::game2d::Game2dEditor> _game2dEditor;
     std::shared_ptr<ObjectStorageGui> _objectsGui;
@@ -41,12 +52,14 @@ private:
     void MenuBar();
     void Workspace();
     void trySaveProject();
+    void PathSelector();
 
     gui_utils::MessagePopup _messagePopup;
-
     ImGui::FileBrowser _filebrowser{ImGuiFileBrowserFlags_CloseOnEsc |
                                     ImGuiFileBrowserFlags_CreateNewDir |
                                     ImGuiFileBrowserFlags_SelectDirectory |
                                     ImGuiFileBrowserFlags_NoTitleBar};
+
+    status _status = NOT_STARTED;
 };
 } // namespace ample::gui
--- gui/src/AmpleGui.cpp
+++ gui/src/AmpleGui.cpp
@@ -33,29 +33,54 @@ AmpleGui::AmpleGui()
 
 void AmpleGui::drawInterface()
 {
-    if (!_game2dEditor)
+    ImGui::SetNextWindowPos({0, 0});
+    ImGui::SetNextWindowSize({static_cast<float>(osWindow().getWidth()),
+                              static_cast<float>(osWindow().getHeight())});
+    ImGui::SetNextWindowBgAlpha(0.0f);
+    if (ImGui::Begin("Ample",
+                     NULL,
+                     ImGuiWindowFlags_MenuBar |
+                         ImGuiWindowFlags_NoCollapse |
+                         ImGuiWindowFlags_NoResize |
+                         ImGuiWindowFlags_NoTitleBar |
+                         ImGuiWindowFlags_NoSavedSettings))
     {
-        ProjectOpener();
+        if (!_game2dEditor)
+        {
+            ProjectOpener();
+        }
+        else
+        {
+            Workspace();
+        }
     }
-    else
-    {
-        Workspace();
-    }
+    PathSelector();
     _messagePopup.drawInterface();
-    _filebrowser.Display();
+    if (_status)
+    {
+        _filebrowser.Display();
+    }
+    ImGui::End();
 }
 
 void AmpleGui::saveProjectAs(const std::filesystem::path &path)
 {
     ASSERT(_game2dEditor);
+    _game2dEditor->setProjectPath(path);
     try
     {
-        _game2dEditor->saveAs(path);
+        _game2dEditor->save();
     }
     catch (const std::exception &e)
     {
         _messagePopup = {"Can't save prject", e.what(), true};
     }
+}
+
+void AmpleGui::saveProject()
+{
+    ASSERT(_game2dEditor);
+    saveProjectAs(_game2dEditor->getProjectPath());
 }
 
 void AmpleGui::openProject(const std::filesystem::path &path)
@@ -67,6 +92,7 @@ void AmpleGui::openProject(const std::filesystem::path &path)
             _game2dEditor->save();
         }
         _game2dEditor = std::make_shared<game::game2d::Game2dEditor>(_window, path);
+        _objectsGui = std::make_shared<ObjectStorageGui>(_game2dEditor);
     }
     catch (const std::exception &e)
     {
@@ -83,6 +109,7 @@ void AmpleGui::createNewProject(const std::filesystem::path &path)
             _game2dEditor->save();
         }
         _game2dEditor = std::make_shared<game::game2d::Game2dEditor>(_window);
+        _objectsGui = std::make_shared<ObjectStorageGui>(_game2dEditor);
         _game2dEditor->setProjectPath(path);
     }
     catch (const std::exception &e)
@@ -91,117 +118,130 @@ void AmpleGui::createNewProject(const std::filesystem::path &path)
     }
 }
 
+void AmpleGui::usePathSelector(status s)
+{
+    if (!s)
+        return;
+    _filebrowser.Open();
+    _status = s;
+}
+
+void AmpleGui::PathSelector()
+{
+    if (!_status)
+    {
+        return;
+    }
+
+    std::filesystem::path path;
+    if (_filebrowser.HasSelected())
+    {
+        path = _filebrowser.GetSelected();
+    }
+    else
+    {
+        return;
+    }
+
+    switch (_status)
+    {
+    case CREATING:
+        createNewProject(path);
+        break;
+
+    case OPENING:
+        openProject(path);
+        break;
+
+    case SAVING_AS:
+        saveProjectAs(path);
+        break;
+
+    case NOT_STARTED:
+        ASSERT(false);
+    }
+    _status = NOT_STARTED;
+}
+
 void AmpleGui::ProjectOpener()
 {
     if (ImGui::Selectable("Create new project"))
     {
+        usePathSelector(CREATING);
     }
 
     if (ImGui::Selectable("Open existing project"))
     {
+        usePathSelector(OPENING);
     }
 }
 
-// void AmpleGui::MenuBar();
-// void AmpleGui::Workspace();
-// void AmpleGui::trySaveProject();
-
 void AmpleGui::MenuBar()
 {
-    // if (ImGui::BeginMenu("File"))
-    // {
-    //     if (ImGui::MenuItem("Save")
-    //     {
-    //         trySaveProject();
-    //     }
-    //     if (ImGui::MenuItem("Save as"))
-    //     {
-    //         _filebrowser.Open();
-    //     }
-    //     ImGui::EndMenu();
-    // }
-    // if (ImGui::BeginMenu("Editor"))
-    // {
-    //     if (ImGui::BeginMenu("Create tab"))
-    //     {
-    //         if (ImGui::MenuItem("State Machine"))
-    //         {
-    //             _editor.openTabCreator(Editor::tabClass::STATE_MACHINE);
-    //         }
-    //         if (ImGui::MenuItem("Level"))
-    //         {
-    //             _editor.openTabCreator(Editor::tabClass::LEVEL);
-    //         }
-    //         ImGui::EndMenu();
-    //     }
-    //     ImGui::EndMenu();
-    // }
-    // if (ImGui::BeginMenu("Help"))
-    // {
-    //     ImGui::Selectable("No");
-    //     ImGui::Selectable("Help");
-    //     ImGui::EndMenu();
-    // }
-    // _filebrowser.Display();
-    // if (_filebrowser.HasSelected())
-    // {
-    //     setProjectPath(_filebrowser.GetSelected());
-    //     trySaveProject();
-    //     _filebrowser.ClearSelected();
-    // }
+    if (ImGui::BeginMenu("Project"))
+    {
+        if (ImGui::MenuItem("Save project"))
+        {
+            saveProject();
+        }
+        if (ImGui::MenuItem("Save project as"))
+        {
+            usePathSelector(SAVING_AS);
+        }
+        if (ImGui::MenuItem("New project"))
+        {
+            usePathSelector(CREATING);
+        }
+        if (ImGui::MenuItem("Open project"))
+        {
+            usePathSelector(OPENING);
+        }
+        ImGui::EndMenu();
+    }
+    if (ImGui::BeginMenu("Help"))
+    {
+        ImGui::Selectable("No");
+        ImGui::Selectable("Help");
+        ImGui::EndMenu();
+    }
 }
 
 void AmpleGui::Workspace()
 {
-    // ImGui::SetNextWindowPos({0, 0});
-    // ImGui::SetNextWindowSize({static_cast<float>(osWindow().getWidth()),
-    //                           static_cast<float>(osWindow().getHeight())});
-    // ImGui::SetNextWindowBgAlpha(0.0f);
-    // if (ImGui::Begin("Ample",
-    //                  NULL,
-    //                  ImGuiWindowFlags_MenuBar |
-    //                      ImGuiWindowFlags_NoCollapse |
-    //                      ImGuiWindowFlags_NoResize |
-    //                      ImGuiWindowFlags_NoTitleBar |
-    //                      ImGuiWindowFlags_NoSavedSettings))
-    // {
+    if (ImGui::BeginMenuBar())
+    {
+        MenuBar();
+        ImGui::EndMenuBar();
+    }
+    ImGui::Columns(3, "Workspace");
 
-    //     if (ImGui::BeginMenuBar())
-    //     {
-    //         MenuBar();
-    //         ImGui::EndMenuBar();
-    //     }
-    //     ImGui::Columns(3, "Workspace");
+    ImGui::BeginChild("Left");
+    if (ImGui::CollapsingHeader("Browser", NULL, 0))
+    {
+        _objectsGui->browser();
+    }
+    if (ImGui::CollapsingHeader("Editor"))
+    {
+        _objectsGui->editor();
+    }
+    ImGui::EndChild();
+    ImGui::NextColumn();
 
-    //     ImGui::BeginChild("Left");
-    //     if (ImGui::CollapsingHeader("Browser", NULL, 0))
-    //     {
-    //         _objectsGui.browser();
-    //     }
-    //     if (ImGui::CollapsingHeader("Editor"))
-    //     {
-    //         _objectsGui.editor();
-    //     }
-    //     ImGui::EndChild();
-    //     ImGui::NextColumn();
+    ImGui::BeginChild("Viewer");
+    _objectsGui->viewer();
+    ImGui::EndChild();
+    ImGui::NextColumn();
 
-    //     ImGui::BeginChild("Viewer");
-    //     _objectsGui.viewer();
-    //     ImGui::EndChild();
-    //     ImGui::NextColumn();
-
-    //     ImGui::BeginChild("Right");
-    //     if (ImGui::CollapsingHeader("Creator"))
-    //     {
-    //         _objectsGui.creator();
-    //     }
-    //     if (ImGui::CollapsingHeader("Inspector"))
-    //     {
-    //         _objectsGui.inspector();
-    //     }
-    //     ImGui::EndChild();
-    //     ImGui::NextColumn();
-    // }
-    // ImGui::End();
+    ImGui::BeginChild("Right");
+    if (ImGui::CollapsingHeader("Creator"))
+    {
+        _objectsGui->creator();
+    }
+    if (ImGui::CollapsingHeader("Inspector"))
+    {
+        _objectsGui->inspector();
+    }
+    ImGui::EndChild();
+    ImGui::NextColumn();
 }
 } // namespace ample::gui
